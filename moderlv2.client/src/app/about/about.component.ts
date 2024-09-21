fad414c26af3ab0b5d34a499e466ea08ddcd90c9
lespuch-v <v.lespukh@gmail.com>
2024-09-21 16:21:50 +0200

    UI works

--- moderlv2.client/src/app/about/about.component.ts
+++ moderlv2.client/src/app/about/about.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { RegistrationModalComponent } from '../registration-modal/registration-modal.component';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  standalone: true,
+  imports: [RegistrationModalComponent]
 })
 export class AboutComponent {
 
+  constructor(public auth: AuthService, private router: Router) {}
+
+  @ViewChild('registrationModal') registrationModal!: RegistrationModalComponent
+
   handleGetStarted() {
-    // if user is logged in - redirect to the main mood page
-    // if user is not logged in - show modal for the registrations.
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['mood']);
+    } else {
+      this.registrationModal.openRegistrationModel();
+    }
   }
 }
